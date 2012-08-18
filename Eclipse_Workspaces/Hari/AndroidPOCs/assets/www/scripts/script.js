6ddb39bce7c4cae5542ed82047591e42d6d4e703
Floyd Pink <imbleedingme@googlemail.com>
2012-08-18 02:44:37 -0400

    some behavior finally
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- Eclipse_Workspaces/Hari/AndroidPOCs/assets/www/scripts/script.js
+++ Eclipse_Workspaces/Hari/AndroidPOCs/assets/www/scripts/script.js
@@ -1,8 +1,14 @@
 var Spinach = (function ($) {
     return {
+        DeviceReady:function () {
+            navigator.notification.alert("PhoneGap is alive and kicking!!");
+        }
     };
 }(jQuery));
 
-$(document).ready(function(){
-
-});
+(function ($) {
+    $(document).ready(function () {
+        //document.addEventListener("deviceready", Spinach.DeviceReady, true);
+        $(document).on("deviceready", Spinach.DeviceReady);
+    });
+}(jQuery));
