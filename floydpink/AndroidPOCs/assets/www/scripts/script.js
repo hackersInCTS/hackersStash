63506ffe0ae887cd3190ec403e0c032d635ac4d3
Floyd Pink <imbleedingme@googlemail.com>
2012-08-19 03:17:50 -0400

    added dialog and geocode
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- floydpink/AndroidPOCs/assets/www/scripts/script.js
+++ floydpink/AndroidPOCs/assets/www/scripts/script.js
@@ -5,7 +5,7 @@ var MapViewModel = function () {
     this.width = 288;
     this.height = 200;
     this.markers = ["New York, NY"];
-    this.sensor = false;
+    this.sensor = true;
     this.getMapUrl = function () {
         return 'https://maps.googleapis.com/maps/api/staticmap?center=' + this.location +
             '&zoom=' + this.zoom + '&size=' + this.width + 'x' + this.height +
@@ -16,14 +16,29 @@ var MapViewModel = function () {
 //Namespaced JS
 var Spinach = Spinach || {};
 
-Spinach.GoogleMaps = (function($){
+Spinach.GoogleMaps = (function ($) {
     return {
-        reverseGeoCode:function(latitude, longitude, onSuccess, onError){
+        geocode:function (address, onSuccess, onError) {
+            var geoCoder = new google.maps.Geocoder();
+            geoCoder.geocode({ 'address':address}, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var location = {
+                        longitude:results[0].geometry.location.lng,
+                        latitude:results[0].geometry.location.lat,
+                        address:results[0].formatted_address
+                    };
+                    onSuccess(location);
+                } else {
+                    onError('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        },
+        reverseGeocode:function (latitude, longitude, onSuccess, onError) {
             var latLong = new google.maps.LatLng(latitude, longitude);
             var geoCoder = new google.maps.Geocoder();
             geoCoder.geocode({
-                'latLng': latLong
-            }, function(results, status) {
+                'latLng':latLong
+            }, function (results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
                     if (results[5]) {
                         return onSuccess(results[5].formatted_address);
@@ -39,10 +54,10 @@ Spinach.GoogleMaps = (function($){
 Spinach.Common = (function ($) {
     return {
         alert:function (message) {
-            try{
+            try {
                 navigator.notification.alert(message, $.noop, "CTS Hackers");
             }
-            catch(e) {
+            catch (e) {
                 alert(message);
             }
         }
@@ -52,24 +67,30 @@ Spinach.Common = (function ($) {
 Spinach.Home = (function ($) {
     return {
         initialize:function () {
-            $(document).on('deviceready', Spinach.Home.deviceReady);
-            $(document).on('click', '#SpecificLocation', Spinach.Home.specificLocationClick);
-            $(document).on('click', '#CurrentLocation', Spinach.Home.currentLocationClick);
+
         },
         deviceReady:function () {
             Spinach.Common.alert("PhoneGap is alive and kicking!!");
         },
-        specificLocationClick:function(){
+        specificLocationOKClick:function () {
             $('#CurrentLocationFlag').val(false);
             Spinach.Home.goToMapPage();
         },
-        currentLocationClick:function(){
+        currentLocationClick:function () {
             $('#CurrentLocationFlag').val(true);
             Spinach.Home.goToMapPage();
         },
-        goToMapPage:function(){
+        goToMapPage:function () {
             Spinach.Map.resetMaps();
             $.mobile.changePage($('#map'));
+        }
+    };
+}(jQuery));
+
+Spinach.Dialog = (function ($) {
+    return {
+        initialize:function(){
+
         }
     };
 }(jQuery));
@@ -77,25 +98,41 @@ Spinach.Home = (function ($) {
 Spinach.Map = (function ($) {
     return {
         initialize:function () {
-            if ($('#CurrentLocationFlag').val() === 'true'){
+            if ($('#CurrentLocationFlag').val() === 'true') {
                 Spinach.Map.getCurrentPosition();
             } else {
-                Spinach.Map.plotMap(new MapViewModel());
+                Spinach.Map.getSpecificLocation();
             }
         },
-        resetMaps:function(){
+        resetMaps:function () {
             $('#mapPlotImg').attr('src', '');
             $('#LocationMarker').empty();
         },
+        getSpecificLocation:function(){
+            var userInput = $('#address').val();
+            $('#address').val('');
+            var onGeocodeSuccess = function(location){
+                var mapViewModel = new MapViewModel();
+                mapViewModel.location = location.latitude + ', ' + location.longitude;
+                mapViewModel.markers = [location.address];
+                $('#LocationMarker').text(location.address);
+            };
+            var onGeocodeError = function (mapViewModel) {
+                return function (errorReason) {
+                    console.log(errorReason);
+                };
+            };
+            Spinach.GoogleMaps.geocode(userInput,onGeocodeSuccess,onGeocodeError);
+        },
         getCurrentPosition:function () {
-            var onReverseGeocodeSuccess = function(mapViewModel){
-                return function(resolvedCity){
+            var onReverseGeocodeSuccess = function (mapViewModel) {
+                return function (resolvedCity) {
                     $('#LocationMarker').text(resolvedCity);
                     Spinach.Map.plotMap(mapViewModel);
                 };
             };
-            var onReverseGeocodeError = function(mapViewModel){
-                return function(errorReason){
+            var onReverseGeocodeError = function (mapViewModel) {
+                return function (errorReason) {
                     console.log(errorReason);
                     Spinach.Map.plotMap(mapViewModel);
                 };
@@ -105,7 +142,7 @@ Spinach.Map = (function ($) {
                 var location = position.coords.latitude + ', ' + position.coords.longitude;
                 mapViewModel.location = location;
                 mapViewModel.markers = [location];
-                Spinach.GoogleMaps.reverseGeoCode(position.coords.latitude,
+                Spinach.GoogleMaps.reverseGeocode(position.coords.latitude,
                     position.coords.longitude,
                     onReverseGeocodeSuccess(mapViewModel),
                     onReverseGeocodeError(mapViewModel));
@@ -114,7 +151,7 @@ Spinach.Map = (function ($) {
                 Spinach.Common.alert('code: ' + error.code + '\n' +
                     'message: ' + error.message + '\n');
             };
-            var geoLocationOptions = { maximumAge:30000, timeout:5000, enableHighAccuracy:true };
+            var geoLocationOptions = { maximumAge:30000, timeout:1000, enableHighAccuracy:true };
             navigator.geolocation.getCurrentPosition(onGetPositionSuccess, onGetPositionError, geoLocationOptions);
         },
         plotMap:function (mapViewModel) {
@@ -128,6 +165,16 @@ $(document).on("pageshow", "#index", function () {
     Spinach.Home.initialize();
 });
 
+$(document).on("pageshow", "#dialog", function () {
+    Spinach.Dialog.initialize();
+});
+
 $(document).on("pageshow", "#map", function () {
     Spinach.Map.initialize();
+});
+
+$(document).ready(function(){
+    $(document).on('deviceready', Spinach.Home.deviceReady);
+    $(document).on('click', '#CurrentLocation', Spinach.Home.currentLocationClick);
+    $(document).on('click', '#PlotSpecificLocationButton', Spinach.Home.specificLocationOKClick);
 });
